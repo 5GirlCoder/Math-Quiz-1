dd05e11d164c638ba6092b379c12280898f03b44
5GirlCoder <73702929+5GirlCoder@users.noreply.github.com>
2021-03-26 17:39:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function FB()
+{
+    window.open("https://www.facebook.com/");
+}
+
+function Insta()
+{
+    window.open("https://www.instagram.com/");
+}
+
+function IN()
+{
+    window.open("https://in.linkedin.com/");
+}
+
+function Tweet()
+{
+    window.open("https://www.twitter.com/?lang=en");
+}
+
+function YT()
+{
+    window.open("https://www.youtube.com/");
+}
